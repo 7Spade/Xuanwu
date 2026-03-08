79ba62d3ee899877f2d34359a1fc173c1ac8fb33
7Spade <7s.i@pm.me>
2026-03-08 19:49:33 +0800

    1

--- src/shared-infra/frontend-firebase/config/firebase.config.ts
+++ src/shared-infra/frontend-firebase/config/firebase.config.ts
@@ -8,8 +8,9 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyBkniZGal_Lls4CR3eFuZvSVMZBe73STNs",
   authDomain: "xuanwu-i-00708880-4e2d8.firebaseapp.com",
+  databaseURL: "https://xuanwu-i-00708880-4e2d8-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "xuanwu-i-00708880-4e2d8",
   storageBucket: "xuanwu-i-00708880-4e2d8.firebasestorage.app",
   messagingSenderId: "65970295651",
-  appId: "1:65970295651:web:0f5d7e391eff98e8c93956"
+  appId: "1:65970295651:web:e97e482f1444afbdc93956"
 };
